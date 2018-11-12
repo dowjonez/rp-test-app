51e705b04a3bb4f6049614d7f776b271ecd04957
Chris Strong <chris-strong@am.com>
2018-11-12 09:33:10 -0600

    nearly completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Observable, of, merge, concat, combineLatest, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ApiService]
 })
 export class AppComponent {
+  @Output() imageEvent = new EventEmitter();
+  @Input() data : Observable<any>;
+  @Input() currentView : string = "list";
+  private favorites : BehaviorSubject<any>;
+  private votes : BehaviorSubject<any>;
   title = 'rp-test-app';
+  
+
+  constructor(private api : ApiService , private cd : ChangeDetectorRef){
+
+  }
+
+  ngOnInit(){
+   this.data = combineLatest(
+     this.api.getImages(),
+     this.api.get("favourites"),
+     this.api.get("votes")
+   )
+   
+   this.data.pipe(
+     map( res=>{
+       let votes = {}
+       res[2].forEach( img => {
+         votes[ img.image_id ] = img
+       })
+       return [res[0], res[1], votes];
+     })
+   ).subscribe(res=>{
+      this.votes = new BehaviorSubject( res[2] )
+      this.favorites = new BehaviorSubject( res[1] )
+   })
+       
+   
+  }
+
+  navigate( test ){
+    this.currentView = test.navigate;
+  }
+
+  onAction($event){
+    if($event.value != null ){
+      
+      let votes =  this.votes.getValue()
+      votes[$event.image_id] = $event
+      this.votes.next( votes )
+      this.cd.detectChanges();
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { NavButtonComponent } from './components/nav-button/nav-button.component';
+import { MediaCellComponent } from './components/media-cell/media-cell.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { FilterPipe } from './pipes/filter.pipe';
+
+import { ApiService } from './services/api.service';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './components/search/search.component';
+import { ListComponent } from './components/list/list.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    NavButtonComponent,
+    MediaCellComponent,
+    FilterComponent,
+    FilterPipe,
+    ProfileComponent,
+    SearchComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, HttpClientModule
+    
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
